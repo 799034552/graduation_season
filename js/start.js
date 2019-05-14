20c937682f47d5f5c27b254560f3f0c1cf4a58da
NeverLCY <2038605015@qq.com>
2019-05-14 18:20:49 +0800

    m初始页接口

--- js/start.js
+++ js/start.js
@@ -1,49 +1,23 @@
-$(document).ready(function () {
-    //白做的部分！
-    // var i = 0;
-    // $("body").delegate(".topic", "click", function () {
-    //         var color = $(this).css('background-color');
-    //         console.log(color);
-    //         // if(color == 'rgb(255, 255, 255)'&&i==5){
-    //         //     alert("只能选五个哦");
-    //         // }
-    //         // 改变选中的主题的样式
-    //         if (color == 'rgb(255, 255, 255)'&&i<5) {
-    //             $(this).css('background-color', 'rgb(126, 125, 125)');
-    //             $(this).find('.chosen').text('1');//判定被选中的元素
-    //             i += 1;
-    //             console.log(i);
-    //         }
-    //         else if(color == 'rgb(126, 125, 125)'){
-    //             $(this).css('background-color', 'white');
-    //             $(this).find('.chosen').text('0');
-    //             i -= 1;
-    //             console.log(i);
-    //         }
-    //         //更改button的样式
-    //         if(i==5){
-    //             $('.choiceButton button').css('background-color', '#1b6aaa');
-    //             $('.choiceButton button').css('color', 'white');
-    //             $('.choiceButton button').text('选好啦');
-    //         }else{
-    //             $('.choiceButton button').css('background-color', 'rgb(179, 178, 178)');
-    //             $('.choiceButton button').css('color', 'gray');
-    //             $('.choiceButton button').text('选择五个话题');
-    //         }
-    // })
+$(document).ready(function () { 
     $('.button button').click(function () {
         $('.background').css('display', 'none');
         $('.bgchoiceTopic').css('display', 'block');
     })
-    // $('.choiceButton button').click(function(){
-    //     if(i!=5){
-    //         alert("要选择5个哦");
-    //     }
-    // })
-
-
-    //引导页面图片滑动
-    var mySwiper = new Swiper('.swiper-container', {
+    //获取微信昵称
+    var name;
+        $.ajax({
+            type:'get',
+            url:baseurl+'/users',
+            success:function(data,status){
+                name=data.name;
+                $('.topic span').text(name);
+            },
+            error:function(){ 
+              
+            }
+        })
+        //引导页面图片滑动
+        var mySwiper = new Swiper('.swiper-container', {
         direction: 'horizontal',
         pagination: {
             el: '.swiper-pagination',
@@ -53,7 +27,7 @@ $(document).ready(function () {
             prevEl: '.swiper-button-prev',
         },
     })
-    $('.leading button').click(function(){
+    $('.leading button').click(function () {
         $('.leading').css('display', 'none');
         $('.background').css('display', 'block');
     })
@@ -102,34 +76,47 @@ $(document).ready(function () {
         )
         $(this).parent().remove();
         var list = $('.userChoiced').children();
-        if(list.length==0){
+        if (list.length == 0) {
             $('.userChoiced').append(
                 ' <p>空空如也~快选些话题吧~</p>'
             )
         }
-        if (list.length <= 4) {//改变button的颜色
+        if (list.length <= 4) {//改变button的文字
             $('.choiceButton button').text('选择五个话题');
             $('.choiceButton button').css('color', 'gray');
         }
     })
 
     //传输话题id
-    $('.choiceButton').click(function(){
+    $('.choiceButton').click(function () {
         var list = $('.userChoiced').children();
-        if(list.length==5){
-    var topic_id1= $('.userChoiced div:nth-child(1) div').text();
-    var topic_id2= $('.userChoiced div:nth-child(2) div').text();
-    var topic_id3= $('.userChoiced div:nth-child(3) div').text();
-    var topic_id4= $('.userChoiced div:nth-child(4) div').text();
-    var topic_id5= $('.userChoiced div:nth-child(5) div').text();
-    console.log(topic_id1);
-    console.log(topic_id2);
-    console.log(topic_id3);
-    console.log(topic_id4);
-    console.log(topic_id5);
-    window.location.href="#.html?topic_id1="+topic_id1+"&topic_id2="+topic_id2+"&topic_id3="+topic_id3+"&topic_id4="+topic_id4
-    +"&topic_id5="+topic_id5;
+        if (list.length == 5) {
+            var topic_id1 = $('.userChoiced div:nth-child(1) div').text();
+            var topic_id2 = $('.userChoiced div:nth-child(2) div').text();
+            var topic_id3 = $('.userChoiced div:nth-child(3) div').text();
+            var topic_id4 = $('.userChoiced div:nth-child(4) div').text();
+            var topic_id5 = $('.userChoiced div:nth-child(5) div').text();
+            console.log(topic_id1);
+            console.log(topic_id2);
+            console.log(topic_id3);
+            console.log(topic_id4);
+            console.log(topic_id5);
+            $.ajax({
+                type:'put',
+                url:baseurl+'/users/collection',
+                data:{
+                    "collection":[topic_id1,topic_id2,topic_id3,topic_id4,topic_id5]
+                },
+                success:function(data,status){
+                   window.location.href = "bubble/index.html";
+                //    window.location.href = "https://graduation2019.100steps.net/auth/jump";
+
+                },
+                error:function(result){ 
+                   alert('请求用户信息失败');
+                }
+            })
         }
-})
+    })
 })
 
